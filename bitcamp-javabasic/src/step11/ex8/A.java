550f8469984bcabec313e483d6f5a5e41088b171
kimkwanhee <rhdwn1955@naver.com>
2018-03-23 12:00:31 +0900

    '''

---
+++ bitcamp-javabasic/src/step11/ex8/A.java
@@ -0,0 +1,6 @@
+package step11.ex8;
+
+public class A /*extends Object*/ {
+    int v1;
+    float v2;
+}
---
+++ bitcamp-javabasic/src/step11/ex8/C.java
@@ -0,0 +1,6 @@
+package step11.ex8;
+
+// 자바는 다중상속을 지원하지 않는다.
+public class C extends A, B {
+    int v3;
+}
