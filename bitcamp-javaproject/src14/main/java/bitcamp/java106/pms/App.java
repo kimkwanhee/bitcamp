f4ce658c8a040f843ebbfebd364c87becc5b3834
kimkwanhee <rhdwn1955@naver.com>
2018-03-23 09:43:23 +0900

    '.'

--- bitcamp-javaproject/src14/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-javaproject/src14/main/java/bitcamp/java106/pms/App.java
@@ -28,8 +28,9 @@ public class App {
 
     public static void main(String[] args) {
         // 클래스를 사용하기 전에 필수 값을 설정한다.
+        
         TeamController teamController = new TeamController(keyScan);
-        MemberController MemberController = new MemberController(keyScan);
+        MemberController memberController = new MemberController(keyScan);
         BoardController boardController = new BoardController(keyScan);
 
         Console.keyScan = keyScan;
@@ -50,16 +51,16 @@ public class App {
             } else if (menu.equals("help")) {
                 onHelp();
             } else if (menu.startsWith("team/")) {
-                TeamController.service(menu, option);
+                teamController.service(menu, option);
             } else if (menu.startsWith("member/")) {
-                MemberController.service(menu, option);
+                memberController.service(menu, option);
             } else if (menu.startsWith("board/")) {
-                BoardController.service(menu, option);
+                boardController.service(menu, option);
             }else {
                 System.out.println("명령어가 올바르지 않습니다.");
             }
 
-            System.out.println();
+            System.out.println(); 
         }
     }
 }
