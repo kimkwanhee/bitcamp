8bf36fc8cc2fe0c093b02474e1cc3ef1ebc0fb49
kimkwanhee <rhdwn1955@naver.com>
2018-04-27 18:12:17 +0900

    +

--- bitcamp-javaproject/src/main/java/bitcamp/java106/pms/dao/BoardDao.java
+++ bitcamp-javaproject/src/main/java/bitcamp/java106/pms/dao/BoardDao.java
@@ -3,8 +3,10 @@ package bitcamp.java106.pms.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 import bitcamp.java106.pms.annotation.Component;
 import bitcamp.java106.pms.domain.Board;
@@ -13,87 +15,54 @@ import bitcamp.java106.pms.jdbc.DataSource;
 @Component
 public class BoardDao {
     
+    SqlSessionFactory sqlSessionFactory;
     DataSource dataSource;
     
-    public BoardDao(DataSource dataSource) {
+    public BoardDao(DataSource dataSource, SqlSessionFactory sqlSessionFactory) {
         this.dataSource = dataSource;
+        this.sqlSessionFactory = sqlSessionFactory;
     }
     
     public int delete(int no) throws Exception {
-        try (
-            Connection con = dataSource.getConnection();
-            PreparedStatement stmt = con.prepareStatement(
-                "delete from pms_board where bno=?");) {
-            
-            stmt.setInt(1, no);
-            return stmt.executeUpdate();
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            int count = sqlSession.delete(
+                    "bitcamp.java106.pms.dao.BoardDao.delete", no);
+            sqlSession.commit();
+            return count;
+        
         } 
     }
     
     public List<Board> selectList() throws Exception {
-        try (
-            Connection con = dataSource.getConnection();
-            PreparedStatement stmt = con.prepareStatement(
-                "select bno,titl,cdt from pms_board");
-            ResultSet rs = stmt.executeQuery();) {
-            
-            ArrayList<Board> arr = new ArrayList<>();
-            while (rs.next()) {
-                Board board = new Board();
-                board.setNo(rs.getInt("bno"));
-                board.setTitle(rs.getString("titl"));
-                board.setCreatedDate(rs.getDate("cdt"));
-                arr.add(board);
-            }
-            return arr;
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            return sqlSession.selectList(
+                    "bitcamp.java106.pms.dao.BoardDao.selectList");
         }
     }
 
     public int insert(Board board) throws Exception {
-        try (
-            Connection con = dataSource.getConnection();
-            PreparedStatement stmt = con.prepareStatement(
-                "insert into pms_board(titl,cont,cdt) values(?,?,now())");) {
-            
-            stmt.setString(1, board.getTitle());
-            stmt.setString(2, board.getContent());
-        
-            return stmt.executeUpdate();
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            int count = sqlSession.insert(
+                    "bitcamp.java106.pms.dao.BoardDao.insert", board);
+            sqlSession.commit();
+            return count;
         }
     }
 
     public int update(Board board) throws Exception {
-        try (
-            Connection con = dataSource.getConnection();
-            PreparedStatement stmt = con.prepareStatement(
-                "update pms_board set titl=?, cont=?, cdt=now() where bno=?");) {
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            int count = sqlSession.update(
+                    "bitcamp.java106.pms.dao.BoardDao.update", board);
+            sqlSession.commit();
+            return count;
             
-            stmt.setString(1, board.getTitle());
-            stmt.setString(2, board.getContent());
-            stmt.setInt(3, board.getNo());
-            return stmt.executeUpdate();
         }
     }
 
     public Board selectOne(int no) throws Exception {
-        try (
-            Connection con = dataSource.getConnection();
-            PreparedStatement stmt = con.prepareStatement(
-                "select bno,titl,cont,cdt from pms_board where bno=?");) {
-            
-            stmt.setInt(1, no);
-            
-            try (ResultSet rs = stmt.executeQuery();) {
-                if (!rs.next()) 
-                    return null;
-                
-                Board board = new Board();
-                board.setNo(rs.getInt("bno"));
-                board.setTitle(rs.getString("titl"));
-                board.setContent(rs.getString("cont"));
-                board.setCreatedDate(rs.getDate("cdt"));
-                return board;
-            }
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+                    return sqlSession.selectOne(
+                            "bitcamp.java106.pms.dao.BoardDao.selectOne", no);
         }  
     }
 }
@@ -107,8 +76,3 @@ public class BoardDao {
 //ver 18 - ArrayList를 이용하여 인스턴스(의 주소) 목록을 다룬다. 
 // ver 16 - 인스턴스 변수를 직접 사용하는 대신 겟터, 셋터 사용.
 // ver 14 - BoardController로부터 데이터 관리 기능을 분리하여 BoardDao 생성.
-
-
-
-
-
