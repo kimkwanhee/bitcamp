0272a7fcd7c6574a5f1189e6c5217d663336336b
kimkwanhee <rhdwn1955@naver.com>
2018-05-01 18:44:01 +0900

    .

---
+++ bitcamp-javaproject/src34/main/java/bitcamp/java106/pms/controller/board/BoardViewController.java
@@ -0,0 +1,51 @@
+// Controller 규칙에 따라 메서드 작성
+package bitcamp.java106.pms.controller.board;
+
+import java.io.PrintWriter;
+
+import bitcamp.java106.pms.annotation.Component;
+import bitcamp.java106.pms.controller.Controller;
+import bitcamp.java106.pms.dao.BoardDao;
+import bitcamp.java106.pms.domain.Board;
+import bitcamp.java106.pms.server.ServerRequest;
+import bitcamp.java106.pms.server.ServerResponse;
+
+@Component("/board/view")
+public class BoardViewController implements Controller {
+    BoardDao boardDao;
+    
+    public BoardViewController(BoardDao boardDao) {
+        this.boardDao = boardDao;
+    }
+    
+    @Override
+    public void service(ServerRequest request, ServerResponse response) {
+        PrintWriter out = response.getWriter();
+        int no = Integer.parseInt(request.getParameter("no"));
+        
+        try {
+            Board board = boardDao.selectOne(no);
+            
+            if (board == null) {
+                out.println("유효하지 않은 게시물 번호입니다.");
+            } else {
+                out.printf("팀명: %s\n", board.getTitle());
+                out.printf("설명: %s\n", board.getContent());
+                out.printf("등록일: %s\n", board.getCreatedDate());
+            }
+        } catch (Exception e) {
+            out.println("상세조회 실패!");
+            e.printStackTrace(out);
+        }
+    }
+}
+
+//ver 31 - JDBC API가 적용된 DAO 사용
+//ver 28 - 네트워크 버전으로 변경
+//ver 26 - BoardController에서 view() 메서드를 추출하여 클래스로 정의.
+//ver 23 - @Component 애노테이션을 붙인다. BoardDao를 받도록 생성자 변경.
+//ver 22 - BoardDao 변경 사항에 맞춰 이 클래스를 변경한다.
+//ver 18 - BoardDao 변경 사항에 맞춰 이 클래스를 변경한다.
+//ver 16 - 인스턴스 변수를 직접 사용하는 대신 겟터, 셋터 사용.
+//ver 14 - BoardDao를 사용하여 게시물 데이터를 관리한다.
+//ver 13 - 게시물 등록할 때 등록일의 문자열을 Date 객체로 만들어 저장.
---
+++ bitcamp-javaproject/src35/main/java/bitcamp/java106/pms/controller/board/BoardAddController.java
@@ -0,0 +1,48 @@
+package bitcamp.java106.pms.controller.board;
+
+import java.io.PrintWriter;
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+import bitcamp.java106.pms.controller.Controller;
+import bitcamp.java106.pms.dao.BoardDao;
+import bitcamp.java106.pms.domain.Board;
+import bitcamp.java106.pms.server.ServerRequest;
+import bitcamp.java106.pms.server.ServerResponse;
+
+@Component("/board/add")
+public class BoardAddController implements Controller {
+    BoardDao boardDao;
+    
+    public BoardAddController(BoardDao boardDao) {
+        this.boardDao = boardDao;
+    }
+     
+    public void service(ServerRequest request, ServerResponse response) {
+        Board board = new Board();
+        board.setTitle(request.getParameter("title"));
+        board.setContent(request.getParameter("content"));
+        board.setCreatedDate(new Date(System.currentTimeMillis()));
+        
+        PrintWriter out = response.getWriter();
+        try {
+            boardDao.insert(board);
+            out.println("등록 성공!");
+        } catch (Exception e) {
+            out.println("등록 실패!");
+            e.printStackTrace(out);
+        }
+    }
+
+}
+
+//ver 31 - JDBC API가 적용된 DAO 사용
+//ver 28 - 네트워크 버전으로 변경
+//ver 26 - BoardController에서 add() 메서드를 추출하여 클래스로 정의.
+//ver 23 - @Component 애노테이션을 붙인다. BoardDao를 받도록 생성자 변경.
+//ver 22 - BoardDao 변경 사항에 맞춰 이 클래스를 변경한다.
+//ver 18 - BoardDao 변경 사항에 맞춰 이 클래스를 변경한다.
+//ver 16 - 인스턴스 변수를 직접 사용하는 대신 겟터, 셋터 사용.
+//ver 14 - BoardDao를 사용하여 게시물 데이터를 관리한다.
+//ver 13 - 게시물 등록할 때 등록일의 문자열을 Date 객체로 만들어 저장.
