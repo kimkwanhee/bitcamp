391c9af468b2c892f35060be8a0ea767a31d4bc4
kimkwanhee <rhdwn1955@naver.com>
2018-06-26 18:57:53 +0900

    .

--- bitcamp-javaproject/src/main/java/bitcamp/java106/pms/web/json/AuthController.java
+++ bitcamp-javaproject/src/main/java/bitcamp/java106/pms/web/json/AuthController.java
@@ -1,6 +1,8 @@
 // 로그인 폼 출력과 사용자 인증처리 서블릿
 package bitcamp.java106.pms.web.json;
 
+import java.util.HashMap;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -30,7 +32,7 @@ public class AuthController {
     }
     
     @RequestMapping("/login")
-    public String login(
+    public Object login(
             @RequestParam("id") String id,
             @RequestParam("password") String password,
             @RequestParam(value="saveId",required=false) String saveId,
@@ -51,38 +53,22 @@ public class AuthController {
         }
         response.addCookie(cookie);
         
+        HashMap<String,Object> result = new HashMap<>();
+        
         if (memberService.isExist(id, password)) { // 로그인 성공!
             session.setAttribute("loginUser", memberService.get(id));
-
-            // 로그인 하기 전의 페이지로 이동한다.
-            String refererUrl = (String)session.getAttribute("refererUrl");
-            
-            if (refererUrl == null || 
-                refererUrl.contains("login.do") ||
-                refererUrl.endsWith("/auth/form.jsp")) { 
-                // 이전 페이지가 없다면 메인 화면으로 이동시킨다.
-                return "redirect:/"; // => "/java106-java-project"
-            } else { 
-                // 이전 페이지가 있다면 그 페이지로 이동시킨다.
-                return "redirect:" + refererUrl;
-            }
-            
+            result.put("state", "success");
         } else { // 로그인 실패!
             session.invalidate();
-            return "auth/fail";
+            result.put("state", "fail");
         }
+        return result;
     }
     
     @RequestMapping("/logout")
-    public String logout(
-            HttpServletRequest request,
-            HttpSession session) throws Exception {
-        
+    public void logout(HttpSession session) throws Exception {
         // 세션을 꺼내 무효화시킨다.
         session.invalidate();
-        
-        // 웹 애플리케이션의 시작 페이지로 가라고 웹브라우저에게 얘기한다.
-        return "redirect:/"; // ==> "/java106-java-project"
     }
 }
 
--- bitcamp-javaproject/src/main/webapp/html4/header.js
+++ bitcamp-javaproject/src/main/webapp/html4/header.js
@@ -1,22 +1,20 @@
 // div#header 태그에 /html/header.html 내용을 삽입한다.
-$.get("/bitcamp-javaproject/html2/header.html", (data) => {
+$.get("/bitcamp-javaproject/html4/header.html", (data) => {
 	$("#header").html(data);
 	loadLoginUser();
 });
 
 function loadLoginUser() {
 	$.getJSON("/bitcamp-javaproject/json/auth/loginUser", (data) => {
-		if (data == "") 
-			location.href = "/bitcamp-javaproject/html2/auth/login.html";
-		else {
 			$("#username").text(data.id);
 			$("#logoutBtn").click((e) => {
 				window.alert("okok");
 				e.preventDefault(); //클릭했을때 하던 일이 있는데 그것을 하지 말라!
 				$.get("/bitcamp-javaproject/json/auth/logout", () => {
-					location.href = "/bitcamp-javaproject/html2/auth/login.html";
+					location.href = "/bitcamp-javaproject/html4/auth/login.html";
 				});
 			});
-		}
+		}).fail(() => {
+		location.href="/bitcamp-javaproject/htm4/auth/login.html";
 	});
 }
